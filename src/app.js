0929b3764c1f96128bf24a147a3e0ed2401b4ae6
Vikas Kumar <vikask54522@gmail.com>
2026-02-13 16:35:51 +0530

    feat: add cookie-parser and cors middleware; update app configuration and create initial model and utility files

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
+
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({extended: true ,limit: '1mb'}));
+app.use(cookieParser());
+app.use(express.static('public'));
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 // require("dotenv").config({path :'/.env'});
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constant.js";
-// import express from "express";
+import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({path: './.env'});
+const app = express();
 
 
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("Error DB connection failed...... ", err);
+});
 
-connectDB();
+
 
 // const app = express();
 // (async () => {
